675a9e80f0a723a52da38161e9b8591bca54f280
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-26 20:04:09 +0200

    authorization elements called in one file

---
+++ dev/app/authorization/authorization.js
@@ -0,0 +1,7 @@
+/* jshint esversion: 6 */
+import authorizationModule from "./authorization.module";
+import authorizationConfig from "./authorization.config";
+import authorizationComponent from "./authorization.component";
+import authorizationService from "./authorization.service";
+import authorizationController from "./authorization.controller";
+import authorizationErrorHandler from "./authorization.errorhandler";
