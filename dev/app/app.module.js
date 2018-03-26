7fd85278de74df8300b8c7c40c77a32b31d91580
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-26 20:25:21 +0200

    authorization, newuser, newroom, rooms called once

--- dev/app/app.module.js
+++ dev/app/app.module.js
@@ -5,7 +5,6 @@ import uiRouter from "@uirouter/angularjs";
 
 const domain = "http://localhost:5000";
 
-
 angular.module("app", [
   ngRoute,
   uiRouter,
@@ -16,42 +15,16 @@ angular.module("app", [
   "roomModule"
  ]);
 
-
-import authorizationModule from "./authorization/authorization.module";
-import authorizationConfig from "./authorization/authorization.config";
-import authorizationComponent from "./authorization/authorization.component";
-import authorizationService from "./authorization/authorization.service";
-import authorizationController from "./authorization/authorization.controller";
-import authorizationErrorHandler from "./authorization/authorization.errorhandler";
-
+import authorizationModule from "./authorization/authorization";
 import newuserModule from "./newuser/newuser";
-// import newuserComponent from "./newuser/newuser.component";
-// import newuserController from "./newuser/newuser.controller";
-
 import newroomModule from "./newroom/newroom";
-// import newroomComponent from "./newroom/newroom.component";
-// import newroomController from "./newroom/newroom.controller";
-
-import roomsModule from "./rooms/rooms.module";
-import roomsComponent from "./rooms/rooms.component";
-import roomsController from "./rooms/rooms.controller";
-
+import roomsModule from "./rooms/rooms";
 import roomModule from "./room/room";
-// import roomModule from "./room/room.module";
-// import roomComponent from "./room/room.component";
-// import roomController from "./room/room.controller";
-
-// import scrabbleModule from "../scrabble/scrabble";
-// import scrabbleDefaults from "./scrabble/scrabble.defaults";
-// import scrabbleService from "./scrabble/scrabble.service";
-// import scrabbleViewService from "./scrabble/scrabble.view.service";
 
 angular.module("app")
 .run(["$trace", function($trace) {$trace.enable("TRANSITION");}])
 .value("route", domain);
 
-  // .factory("_", ["$window", lodashFactory])
-  //  .directive("verifypass", ["$scope", verifypassDirective])"
-  // import has from "lodash/has";
-  // import verifypassDirective from "./authorization/verifypass.directive";
-  // import lodashFactory from "./core/lodash.factory";
+// .factory("_", ["$window", lodashFactory])
+// import has from "lodash/has";
+// import lodashFactory from "./core/lodash.factory";
