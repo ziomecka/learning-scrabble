8c298cedbc5bcf444cbe60ba0c908bb6805c9492
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-26 20:18:25 +0200

    newroomDefaults added

--- dev/app/newroom/newroom.js
+++ dev/app/newroom/newroom.js
@@ -2,3 +2,4 @@
 import newroomModule from "./newroom.module";
 import newroomController from "./newroom.controller";
 import newroomComponent from "./newroom.component";
+import newroomDefaults from "./newroom.defaults";
