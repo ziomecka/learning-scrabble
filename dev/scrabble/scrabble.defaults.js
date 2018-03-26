42d769227e2e5343b329b2cade0001030ae7a310
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-26 20:21:10 +0200

    defaults moved to newroomDefaults

--- dev/scrabble/scrabble.defaults.js
+++ dev/scrabble/scrabble.defaults.js
@@ -1,11 +1,5 @@
 /* jshint esversion: 6 */
 angular
-.module("scrabbleModule")
-.value("scrabbleDefaults", {
-  name: "scrabble",
-  owner: {
-    name: "Player"
-  },
-  placesOptions: [2, 3, 4],
-  timeOptions: [3, 5, 7, 10, 15]
-});
+  .module("scrabbleModule")
+  .value("scrabbleDefaults", {
+  });
