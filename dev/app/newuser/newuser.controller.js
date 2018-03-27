0c26df5328b396bb1d8b19b987c13d49c36366aa
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-27 23:30:26 +0200

    changes to rootscope removed (rootscope's authorized and user) authorizationService's authorize method used

--- dev/app/newuser/newuser.controller.js
+++ dev/app/newuser/newuser.controller.js
@@ -10,18 +10,18 @@ angular
   "authorizationService",
   "clientEvents",
   ($scope, $rootScope, $location, $state, clientService, authorizationService, clientEvents) => {
-  const me = $scope;
-  let loginNotUnique = false;
-  me.login = "";
-  me.password = "";
+    const me = $scope;
+    let loginNotUnique = false;
+    me.login = "";
+    me.password = "";
 
-  me.createUser = data => clientService.emit(clientEvents.reqNewuser, data);
-  me.loginUser = () => authorizationService.go("authorization");
-  clientService.on("auth: login already exists", () => loginNotUnique = true);
+    me.createUser = data => clientService.emit(clientEvents.reqNewuser, data);
+    me.loginUser = () => authorizationService.go("authorization");
+    clientService.on("auth: login already exists", () => loginNotUnique = true);
 
-  clientService.on("auth: user created", () => {
-    $rootScope.user = me.login;
-    $rootScope.authorized = true;
-    authorizationService.go({login: me.login});
-  });
-}]);
+    clientService.on(clientEvents.resNewuser, data => {
+      authorizationService.authorize(data);
+      authorizationService.go();
+    });
+  }
+]);
