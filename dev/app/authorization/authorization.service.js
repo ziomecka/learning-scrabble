b8d2ef008e657691ec2185be3ac546835e84bf9d
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-27 23:20:15 +0200

    authorize method added, go method now only for changing state,

--- dev/app/authorization/authorization.service.js
+++ dev/app/authorization/authorization.service.js
@@ -4,23 +4,24 @@ angular
 .service("authorizationService", function ($state) {
   this.authorized = true;
   this.guest = false;
-  this.login = undefined;
+  this.login = "";
 
   this.clear = () => {
     this.authorized = false;
     $state.go("authorization");
   };
 
-  this.go = data => {
-    let state;
+  this.authorize = data => {
     ({
       authorized: this.authorized = true,
       guest: this.guest = false,
-      login: this.login,
-      state: state = "private.rooms"
+      login:  this.login = ""
     } = data);
-    let params = {};
-    params.roomId = data.roomId;
-    $state.go(state, params);
+  };
+
+  this.go = data => {
+    data = Object(data);
+    let {state: state = "private.rooms", roomId: roomId = ""} = data;
+    $state.go(state, {"roomId": roomId});
   };
 });
