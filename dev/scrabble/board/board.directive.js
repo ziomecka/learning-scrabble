ebbbae4d58137024806a727e8121ddfcfb13823d
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-03-27 23:17:34 +0200

    first row and first column added

--- dev/scrabble/board/board.directive.js
+++ dev/scrabble/board/board.directive.js
@@ -1,66 +1,75 @@
 /* jshint esversion: 6 */
 angular
-.module("boardModule")
-.directive("boardDirective", [
-  "boardOptions",
-  "boardService",
-  (boardOptions, boardService) => {
-    let {row: row, field: field} = boardOptions.view;
-    let {size, bonuses, view} = boardOptions;
+  .module("boardModule")
+  .directive("boardDirective", [
+    "boardOptions",
+    "boardService",
+    (boardOptions, boardService) => {
+      let {row: row, field: field} = boardOptions.view;
+      let {size, bonuses, view} = boardOptions;
 
-    let fieldSVG =
-      `<svg xmlns='http://www.w3.org/2000/svg' height="100%" width="100%">
-        <rect height="100%" width="100%"></rect>
-      </svg>`;
+      let fieldSVG =
+        `<svg xmlns='http://www.w3.org/2000/svg' height="100%" width="100%">
+          <rect height="100%" width="100%"></rect>
+        </svg>`;
 
-    let templatePattern = options => {
-      let {element, attributes} = options;
-      let {id, className} = view[element];
-      // let id  = viewOptions[element].id;
-      // let className = viewOptions[element].className;
-      let templates = {
-        idClassName: (inner, i) => `<div id="${id}-${i}" class="${className}">
-                                ${inner}
-                              </div>`,
-        className: (inner) => `<div class="${className}">
-                                ${inner}
-                              </div>`,
-        idClassNameMod: (inner, i, mod) => `<div id="${id}-${i}" class="${className} ${className}--${mod}">
-                                ${inner}
-                              </div>`,
-        classNameMod: (inner, mod) => `<div class="${className} ${className}--${mod}">
-                                ${inner}
-                              </div>`,
-        id: (inner, i) => `<div id="${id}-i">
-                                ${inner}
-                              </div>`
+      let templatePattern = options => {
+        let {element, attributes} = options;
+        let {id, className} = view[element];
+        let templates = {
+          idClassName: (inner, i) => `<div id="${id}-${i}" class="${className}">
+                                  ${inner}
+                                </div>`,
+          className: (inner) => `<div class="${className}">
+                                  ${inner}
+                                </div>`,
+          idClassNameMod: (inner, i, mod) => `<div id="${id}-${i}" class="${className} ${className}--${mod}">
+                                  ${inner}
+                                </div>`,
+          classNameMod: (inner, mod) => `<div class="${className} ${className}--${mod}">
+                                  ${inner}
+                                </div>`,
+          id: (inner, i) => `<div id="${id}-i">
+                                  ${inner}
+                                </div>`
+        };
+        return templates[attributes];
       };
-      return templates[attributes];
-    };
 
-    let prepareBoard = () => {
-      let fieldTemplate = templatePattern({element: "field", attributes: "idClassNameMod"});
-      let rowTemplate = templatePattern({element: "row", attributes: "idClassName"});
-      let boardTemplate = templatePattern({element: "board", attributes: "idClassName"});
+      let prepareBoard = () => {
+        let firstRowTemplate = templatePattern({element: "firstRow", attributes: "className"});
+        let descriptionFieldTemplate = templatePattern({element: "descriptionField", attributes: "className"});
+        let fieldTemplate = templatePattern({element: "field", attributes: "idClassNameMod"});
+        let rowTemplate = templatePattern({element: "row", attributes: "idClassName"});
+        let boardTemplate = templatePattern({element: "board", attributes: "idClassName"});
 
-      let fields = "";
-      let rows = "";
-      let board = "";
+        let fields = "";
+        let rows = "";
+        let board = "";
 
-      for (let i = 0; i < size; i++) {
+        /** Prepare the first row with letters. */
+        let charCode = 97;
+        fields += descriptionFieldTemplate("");
         for (let j = 0; j < size; j++) {
-          let bonus = boardService.fields[i][j].bonus;
-            fields += fieldTemplate(fieldSVG, `${i}-${j}`, bonus);
+          fields += descriptionFieldTemplate(String.fromCharCode(charCode++));
         }
-        rows += rowTemplate(fields, i);
-        fields = "";
-      }
-      return boardTemplate(rows, 1);
-    };
 
-    return {
-      // template: "<div>dupa</div>"
-      template: prepareBoard()
-    };
-  }
-]);
+        rows += firstRowTemplate(fields);
+
+        for (let i = 0; i < size; i++) {
+          /** Prepare the first column with numbers. */
+          fields = descriptionFieldTemplate(i + 1);
+          for (let j = 0; j < size; j++) {
+            let bonus = boardService.fields[i][j].bonus;
+              fields += fieldTemplate(fieldSVG, `${i}-${j}`, bonus);
+          }
+          rows += rowTemplate(fields, i);
+        }
+        return boardTemplate(rows, 1);
+      };
+
+      return {
+        template: prepareBoard()
+      };
+    }
+  ]);
