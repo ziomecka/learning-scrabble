bf82859e4b82108fd43d79fe5f110bfda0681d21
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:46:27 +0200

    Files either deleted or moved to other directories

--- dev/server/server.player.controller.js
+++
@@ -1,6 +0,0 @@
-/* jshint esversion: 6 */
-/** One object per each client connected to the server.
-    Handles incoming messages from the client.
-    Performs actions in the game on behalf of the player.
-    Periodically sends messages containing updates of the state of the game to the client.
-    */
