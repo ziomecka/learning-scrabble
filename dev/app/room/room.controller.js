d70440bcb501062e9f0a67be8e1414de236f8307
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:07:53 +0200

    appService used for communication with server scrabbleService added Get up method added

--- dev/app/room/room.controller.js
+++ dev/app/room/room.controller.js
@@ -1,65 +1,88 @@
 /* jshint esversion: 6 */
-
-angular
-.module("roomModule")
-.controller("roomController", [
+module.exports = [
   "$scope",
-  "$rootScope",
-  "clientService",
+  "scrabbleService",
+  "appService",
   "newroomDefaults",
   "authorizationService",
-  "clientEvents",
-    function ($scope, $rootScope, clientService, newroomDefaults, authorizationService, clientEvents) {
-      let me = $scope;
-      me.defaults = newroomDefaults;
-      me.time = newroomDefaults.timeOptions[3] || 10;
-      me.places = [];
-      me.players = [];
-      me.login = authorizationService.login;
+  function ($scope, scrabbleService, appService, newroomDefaults, authorizationService) {
+    let me = $scope;
+    me.defaults = newroomDefaults;
+    me.time = newroomDefaults.timeOptions[3] || 10;
+    me.places = [];
+    me.players = [];
+    me.login = authorizationService.login; // TODO needed?
+    me.placeId = "";
+    me.seated = false;
 
-      this.$onInit = () => {
-        ({places: $scope.places, id: $scope.id, numberPlaces: $scope.numberPlaces} = JSON.parse(this.roomData));
-        clientService.emit(clientEvents.reqCreateScrabble);
-      };
+    // this.$onInit = () => {
+    //   ({
+    //     places: $scope.places,
+    //     id: $scope.id,
+    //     numberPlaces: $scope.numberPlaces,
+    //     name: $scope.name
+    //   } = JSON.parse(this.roomData));
+    //   me.place = me.findPlace();
+    //   me.seated = me.doISeat();
+    // };
 
-      clientService.on(clientEvents.resCreateScrabbleSuccess, data => {
-        console.log(`I have been informed about creating game with id: ${data.id}.`);
+    me.findPlace = () => me.places.findIndex((place) => place.login === me.login);
+
+    me.doISeat = () => me.place >= 0;
+
+    me.numberPlacesChanged = number => {
+      appService.changeNumberPlaces({
+        id: me.id,
+        number: number,
+        callback: {
+          successIncreased: data => me.places = [...me.places, ...data.places],
+        }
       });
+    };
 
-      me.seated = (() => me.places.some((place) => place.login === me.login))();
-
-      me.numberPlacesChanged = num => {
-        clientService.emit(clientEvents.reqNumberPlacesChanged, {
-          id: me.id,
-          number: num
-        });
-      };
-
-      clientService.on(clientEvents.resPlacesAdded, data => {
-        me.places = [...me.places, ...data.places];
-        console.log("New places created");
+    me.takePlace = data => {
+      appService.takePlace({
+        roomID: me.id,
+        placeId: data.placeId,
+        callback: {
+          successTakePlace: data => {
+            let place = me.places.find(item => item.id === data.placeId);
+            place.owner.login = data.login;
+            place.isOpened = false;
+            me.seated = true;
+            me.players.push(data.login);
+            me.askForAcceptance = false;
+            place = null;
+          },
+        }
       });
+    };
 
-      me.takePlace = data => {
-        console.log("I try to take place");
-        clientService.emit(clientEvents.reqTakePlace, {
-          roomID: me.id,
-          placeID: data.placeID,
-          login: me.login
-        });
-      };
-
-      clientService.on("room: player took place", data => {
-          console.log("I took place");
-          let place = me.places.find(item => item.id === data.placeID);
-          // place.owner.id = data.ownerID;
-          // place.owner.name = data.ownerName;
-          // place.name = data.ownerName;
-          place.reserved = false;
-          place.isOpened = false;
-          me.seated = true;
-          me.players.push(data.login);
-          place = null;
+    me.getUp = () => {
+      place.owner.login = undefined;
+      place.name = "...";
+      place.isOpened = true;
+      me.seated = false;
+      me.players.splice(me.players.findIndex(me.login), 1);
+      appService.getUp({
+        roomID: me.id,
+        placeId: me.place,
+        callback: {
+          successGetUp: () => {}
+        }
       });
+    };
+
+    me.listenToStart = (() => appService.askPlayerStart({
+      callback: {
+        successAskPlayerStart: () => me.askForAcceptance = true
+      }
+    }))();
+
+    me.listenToCreateScrabble = (() => scrabbleService.createScrabble({
+      callback: {
+        successCreateScrabble: () => {} //TODO
+      }
+    }))();
   }
-]);
+];
