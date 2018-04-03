6e1fdd67d20f721f1080b5a9b705285dca3c4fa2
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:26:15 +0200

    ngCookies and ngAnimate included newRoom module removed

--- dev/app/app.js
+++ dev/app/app.js
@@ -1,5 +1,15 @@
 /* jshint esversion: 6 */
-import appModule from "./app.module";
-import appController from "./app.controller";
-import clientService from "./client/client.service";
-import clientEvents from "./client/client.events";
+import "./app.module";
+import "./app.controller";
+
+import "./title/title";
+
+import "./client/app.events";
+import "./client/scrabble.events";
+
+import "./client/socket.service";
+import "./client/app.service";
+import "./client/scrabble.service";
+
+import "./app.run";
+import "./home/home";
--- dev/app/app.module.js
+++ dev/app/app.module.js
@@ -2,27 +2,29 @@
 import angular from "angular";
 import ngRoute from "angular-route";
 import uiRouter from "@uirouter/angularjs";
+import ngCookies from "angular-cookies";
+import ngAnimate from "angular-animate";
 
 const domain = "http://localhost:5000";
 
 angular.module("app", [
   ngRoute,
   uiRouter,
+  ngCookies,
+  ngAnimate,
   "authorizationModule",
   "newuserModule",
-  "newroomModule",
   "roomsModule",
   "roomModule"
  ]);
 
 import authorizationModule from "./authorization/authorization";
 import newuserModule from "./newuser/newuser";
-import newroomModule from "./newroom/newroom";
 import roomsModule from "./rooms/rooms";
 import roomModule from "./room/room";
 
 angular.module("app")
-.run(["$trace", function($trace) {$trace.enable("TRANSITION");}])
+// .run(["$trace", function($trace) {$trace.enable("TRANSITION");}])
 .value("route", domain);
 
 // .factory("_", ["$window", lodashFactory])
