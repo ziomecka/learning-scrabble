857e961fbaeb3599deee4674d2a3b561dac36aec
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:35:38 +0200

    Unauthorize method added. Auhtorization states added. Go method default state: either rooms or home depending on user being authorized.

--- dev/app/authorization/authorization.service.js
+++ dev/app/authorization/authorization.service.js
@@ -1,27 +1,44 @@
 /* jshint esversion: 6 */
 angular
 .module("authorizationModule")
-.service("authorizationService", function ($state) {
-  this.authorized = true;
-  this.guest = false;
-  this.login = "";
-
-  this.clear = () => {
+.service("authorizationService", [
+  "$state",
+  "authorizationStates",
+  "authorizationCookiesService",
+  function ($state, authorizationStates, authorizationCookiesService) {
     this.authorized = false;
-    $state.go("authorization");
-  };
+    this.guest = false;
+    this.login = "";
+    this.lastTo = "";
 
-  this.authorize = data => {
-    ({
-      authorized: this.authorized = true,
-      guest: this.guest = false,
-      login:  this.login = ""
-    } = data);
-  };
+    this.clear = () => {
+      this.authorized = false;
+      $state.go(authorizationStates.authorization);
+    };
 
-  this.go = data => {
-    data = Object(data);
-    let {state: state = "private.rooms", roomId: roomId = ""} = data;
-    $state.go(state, {"roomId": roomId});
-  };
-});
+    this.authorize = data => {
+      ({
+        authorized: this.authorized = true,
+        guest: this.guest = false,
+        login:  this.login = ""
+      } = data);
+    };
+
+    this.unauthorize = () => {
+      this.authorized = false;
+      this.login = "";
+      // TODO change name of the below service
+      /** Destroy cookies */
+      authorizationCookiesService.removeAuthorizationCookies();
+      /** Go back to authorization state */
+      this.go({state: authorizationStates.authorization});
+    };
+
+    // TODO some bullshit when trying to enter room that does not exist
+    this.go = data => {
+      data = Object(data);
+      let {state: state = this.authorized? authorizationStates.rooms: authorizationStates.home, roomId: roomId = ""} = data;
+      $state.go(state, {"roomId": roomId});
+    };
+  }]
+);
