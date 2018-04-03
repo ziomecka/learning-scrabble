885422a5d5135bb567a15a31114683ceb4deba56
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:09:56 +0200

    Formatting

--- dev/app/room/room.module.js
+++ dev/app/room/room.module.js
@@ -1,4 +1,7 @@
 /* jshint esversion: 6 */
 import angular from "angular";
-// import screabbleModule from "../../scrabble/scrabble";
-angular.module("roomModule", ["scrabbleModule"]);
+import scrabbleModule from "../../scrabble/scrabble";
+
+angular.module("roomModule", [
+  "scrabbleModule"
+]);
