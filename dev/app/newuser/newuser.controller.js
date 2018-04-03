c60a757b343274ba655034f0fa23de7cee057b74
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:03:10 +0200

    Initialized via ui-router. appService for communictaion with server added (socket.io) authorizationStates added.

--- dev/app/newuser/newuser.controller.js
+++ dev/app/newuser/newuser.controller.js
@@ -1,27 +1,27 @@
 /* jshint esversion: 6 */
-angular
-.module("newuserModule")
-.controller("newuserController", [
+module.exports = [
   "$scope",
-  "$rootScope",
-  "$location",
-  "$state",
-  "clientService",
+  "appService",
   "authorizationService",
-  "clientEvents",
-  ($scope, $rootScope, $location, $state, clientService, authorizationService, clientEvents) => {
+  "authorizationStates",
+  ($scope, appService, authorizationService, authorizationStates) => {
     const me = $scope;
     let loginNotUnique = false;
     me.login = "";
     me.password = "";
 
-    me.createUser = data => clientService.emit(clientEvents.reqNewuser, data);
-    me.loginUser = () => authorizationService.go("authorization");
-    clientService.on("auth: login already exists", () => loginNotUnique = true);
+    me.goAuthorization = () => {
+      authorizationService.go({state: authorizationStates.authorization});
+    };
 
-    clientService.on(clientEvents.resNewuser, data => {
-      authorizationService.authorize(data);
-      authorizationService.go();
-    });
+    me.createUser = data => {
+      const failureLogin = () => loginNotUnique = true;
+      appService.createUser({
+        data: data,
+        callback: {
+          failureLogin: failureLogin
+        }
+      });
+    };
   }
-]);
+];
