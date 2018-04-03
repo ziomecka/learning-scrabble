c73ebe906d2524bcbd822103865bb84220171d6d
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:41:25 +0200

    First commit

---
+++ dev/server/app/app.place.js
@@ -0,0 +1,30 @@
+/* jshint esversion: 6 */
+const UUID = require("uuid");
+
+export default class Player {
+  constructor(options) {
+    options = Object(options);
+    ({login: this.login, id: this.id = UUID()} = options);
+  }
+
+  get isOpened() {
+    return !this.login;
+  }
+
+  setOwner(data) {
+    if (this.isOpened) {
+      ({login: this.login} = data);
+      return this;
+    } else {
+      return false;
+    }
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      login: this.login,
+      isOpened: this.isOpened
+    };
+  }
+}
