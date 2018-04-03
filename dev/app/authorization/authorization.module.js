618b8191c1d9b2316e82ec7fc8c7502b05c7ff11
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:29:07 +0200

    CookiesMOdule added. Formatting improved.

--- dev/app/authorization/authorization.module.js
+++ dev/app/authorization/authorization.module.js
@@ -1,5 +1,10 @@
 /* jshint esversion: 6 */
 import ngRoute from "angular-route";
 import uiRouter from "@uirouter/angularjs";
+import cookiesModule from "../cookies/cookies";
 
-angular.module("authorizationModule", [ngRoute, uiRouter]);
+angular.module("authorizationModule", [
+  ngRoute,
+  uiRouter,
+  "cookiesModule"
+]);
