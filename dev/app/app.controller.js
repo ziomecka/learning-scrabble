1ef0abe0c7a5d102bd68f4e40ddcfa41e068510d
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:24:05 +0200

    appService used for communication with appService properties for title introduced (narrowTitle and shortTitle)

--- dev/app/app.controller.js
+++ dev/app/app.controller.js
@@ -1,21 +1,35 @@
 /* jshint esversion: 6 */
-
+// TODO create new room does not work!
+// console console error
+// + unhandled promise rejections
+// transport pooling https://stackoverflow.com/questions/28261546/transport-polling-error-with-socket-io
 angular
-.module("app")
-.controller("appController", [
-  "$scope",
-  "$rootScope",
-  "clientService",
-  "authorizationService",
-  "clientEvents",
-  ($scope, $rootScope, clientService, authorizationService, clientEvents) => {
-  let me = $scope;
-  me.user = "";
-  me.$watch("authorized", newValue => me.authorized = newValue);
-  me.authorized = authorizationService.authorized;
-  me.guest = $rootScope.guest;
-  me.logout = () => authorizationService.clear();
-  clientService.on(clientEvents.resConnected, data => console.log("I'm connected"));
-  clientService.on("disconnect", () => console.log("I have been disconnected"));
-  //clientService.getSocket().removeAllListeners();
-}]);
+  .module("app")
+  .controller("appController", [
+    "$scope",
+    "$rootScope",
+    "appService",
+    "authorizationService",
+    "$timeout",
+    "authorizationGlobals",
+    "$state",
+    ($scope, $rootScope, appService, authorizationService, $timeout, authorizationGlobals, $state) => {
+      let me = $scope;
+      me.$state = $state;
+      $rootScope.narrowTitle = false;
+      me.narrowTitle = $rootScope.narrowTitle;
+      $rootScope.$watch("narrowTitle", newValue => me.narrowTitle = newValue);
+      /** Sets the standardTitle to tru after 2.5 seconds.
+          Ui-router resolves the states with 3s delay
+          (only these states that may occur after 'home' state, i.e.
+          'authorization' state. That's why it is done via ui-router, not css).
+          If standardTitle === true then
+          the ng-class adds the '--short' class to the title
+          that makes the title shorter. */
+      $timeout(() => {
+        me.shortTitle = true;
+      }, authorizationGlobals.shorterTitleTime);
+      me.logout = () => authorizationService.clear();
+      appService.connection();
+    }
+  ]);
