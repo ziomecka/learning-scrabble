786b487e902d70d992bc08eb016800ed44e3d6d1
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:26:40 +0200

    First commit. Deals with 'not authorized' rejection for tansition. To be verified if needed.

--- dev/app/authorization/authorization.errorhandler.js
+++ dev/app/authorization/authorization.errorhandler.js
@@ -1,11 +1,13 @@
 /* jshint esversion: 6 */
+// TODO - validate if needed
 angular
-.module("authorizationModule")
-.run([
-  "$transitions",
-  "$state",
-  ($transitions, $state) => {
-  $transitions.onError({}, function (trans) {
-    if (trans.error().detail  === "not authorized") $state.go("authorization");
-  });
-}]);
+  .module("authorizationModule")
+  .run([
+    "$transitions",
+    "$state",
+    ($transitions, $state) => {
+      $transitions.onError({}, function (trans) {
+        if (trans.error().detail  === "not authorized") $state.go("authorization");
+      });
+    }
+  ]);
