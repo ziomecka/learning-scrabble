8dc093311f1b8a1524d94893ef0e76bce79b4ba0
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:17:54 +0200

    First commit

---
+++ dev/app/authorization/constants/authorization.globals.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+/** Used for animation of the 'scrabble' title. */
+angular
+  .module("authorizationModule")
+  .constant("authorizationGlobals", {
+    shorterTitleTime: 800,
+    delayedViewTime: 810
+  });
