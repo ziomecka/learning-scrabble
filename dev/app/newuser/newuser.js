cebce9e54a153d67c39e17342628f9128a454daf
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-03 20:03:41 +0200

    Controller and Compponent removed  (initialised via ui-router)

--- dev/app/newuser/newuser.js
+++ dev/app/newuser/newuser.js
@@ -1,5 +1,3 @@
 /* jshint esversion: 6 */
-import newuserModule from "./newuser.module";
-import newuserController from "./newuser.controller";
-import newuserComponent from "./newuser.component";
-import newuserPasswordsDirective from "./compare.passwords.directive";
+import "./newuser.module";
+import "./compare.passwords.directive";
