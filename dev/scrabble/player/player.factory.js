c9a1cc129bea9af7c27b6d3d52c12f4625d527e2
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-04 14:52:37 +0200

    rack removed methods added

--- dev/scrabble/player/player.factory.js
+++ dev/scrabble/player/player.factory.js
@@ -2,20 +2,61 @@
 angular
   .module("playerModule")
   .factory("playerFactory", [
-    "rackFactory",
-    (rackFactory) => {
+    "scrabbleTalk",
+    "lodashFactory",
+    function (scrabbleTalk, lodashFactory) {
       class Player {
         constructor (playerOptions) {
           ({name: this.name, time: this.time, id: this.id} = playerOptions);
           this.points = 0;
-          this.rack = new rackFactory();
+          this.tiles = [];
+
+          scrabbleTalk.listenInitialTiles({
+            callbacks: {
+              initialTiles: data => player.getTiles(data)
+            }
+          });
+          scrabbleTalk.listenRoundStarted({
+            callbacks: {
+              roundStarted: data => player.roundStarted(data)
+            }
+          });
         }
 
-        getsTile (tile) {
-          tile.onRack = true;
-          this.rack.getsTile(tile);
+        getTiles(arr) {
+          tiles = [...tiles, ...arr];
         }
+
+        roundStarted () {
+        }
+
+        removeTiles (options) {
+          let {property, value} = options;
+          return lodashFactory.remove(tiles, (item, index, array) => {
+            return item[property] === value;
+          });
+        }
+
+        exchangeTiles (data) {
+          scrabbleTalk.exchangeTiles({
+            callbacks: {
+              exchangedTiles: data => getTiles(data)
+            },
+            tiles: removeTiles({
+              property: toBeExchanged,
+              value: true
+            })
+          });
+        }
+
+        endRound (data) {
+        }
+
+        verifyWord () {
+        }
+
       }
+
       return Player;
     }
   ]);
