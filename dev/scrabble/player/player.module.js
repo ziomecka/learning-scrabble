0c2bca0243a0c4842e717e03561e9f64d9fbe885
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-04 14:53:18 +0200

    rackModule removed

--- dev/scrabble/player/player.module.js
+++ dev/scrabble/player/player.module.js
@@ -1,5 +1,4 @@
 /* jshint esversion: 6 */
 import angular from "angular";
-import rackModule from "../rack/rack.js";
 
-angular.module("playerModule", ["rackModule"]);
+angular.module("playerModule", []);
