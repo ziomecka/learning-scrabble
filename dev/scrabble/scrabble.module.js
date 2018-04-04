a832f61a0a2a08efa05325dc53027075772a9caa
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-04 14:56:57 +0200

    formatting controls module included

--- dev/scrabble/scrabble.module.js
+++ dev/scrabble/scrabble.module.js
@@ -1,8 +1,14 @@
 /* jshint esversion: 6 */
 import angular from "angular";
 
-angular.module("scrabbleModule", ["boardModule", "bagModule", "playerModule"]);
+angular.module("scrabbleModule", [
+  "boardModule",
+  // "bagModule",
+  "playerModule",
+  "controlsModule"
+]);
 
-import boardModule from "./board/board";
-import bagModule from "./bag/bag";
+import controlsModule from "./controls/controls";
 import playerModule from "./player/player";
+import boardModule from "./board/board";
+// import bagModule from "./bag/bag";
