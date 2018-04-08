095bdac27de81153c000e88ffe3c2ab491ec1345
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-08 18:20:30 +0200

    PlayerFactory included. Functions changed: drawTiles, prepare, createScrabble, start.

--- dev/scrabble/scrabble.game.factory.js
+++ dev/scrabble/scrabble.game.factory.js
@@ -3,27 +3,63 @@ angular
 .module("scrabbleModule")
 .factory("scrabbleGameFactory", [
   "scrabbleTalk",
-  (scrabbleTalk) => {
+  "playerFactory",
+  "$q",
+  (scrabbleTalk, playerFactory, $q) => {
+
     class ScrabbleGame {
       constructor (options) {
         let {tiles: tiles} = options;
-        me.player = playerFactory({tiles: tiles});
+        this.player = new playerFactory({tiles: tiles});
+        options.resolve();
+      }
+
+      drawTiles (options) {
+        let deferred = $q.defer();
+        scrabbleTalk.drawTiles({
+          gameId: this.id,
+          number: options.number,
+          callbacks: {
+            success: data => deferred.resolve(this.player.getTiles(data))
+          }
+        });
+        return deferred.promise;
       }
     }
 
-    const prepare = options => {
+    const prepare = function(options) {
+      let data = {};
+      ({data: {id: data.id}} = options);
+      let deferred = $q.defer();
+
       scrabbleTalk.createScrabble({
         callbacks: {
-          scrabbleCreated: data => data // TODO
-        }
+          success: data => {
+            console.log(`resolved here`);
+            deferred.resolve(data);
+          },
+          failure: data => {} // TODO
+        },
+        data: data
       });
+      return deferred.promise;
+    };
+
+    const start = function(options) {
+      options = Object(options);
+      let deferred = $q.defer();
+      deferred.resolve(new ScrabbleGame(options));
+      return deferred.promise;
     };
 
     let destroy = null; // TODO ?
 
     return {
       prepare: prepare,
-      start: options => new ScrabbleGame(options)
+      start: options => {
+        options = Object(options);
+        start(options);
+      }
     };
   }
 ]);
