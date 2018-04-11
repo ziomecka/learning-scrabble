5e4366fbfa07eb2d56151afdc1169e4181332b5e
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-11 13:53:34 +0200

    Handlers added to SocketService

--- dev/app/socket/socket.service.js
+++ dev/app/socket/socket.service.js
@@ -21,6 +21,7 @@ angular
         socket.off(event);
       };
 
+
       this.emit = (event, data, callback) => {
         socket.emit(event, data, () => {
           const args = arguments;
@@ -28,6 +29,71 @@ angular
             callback.apply(socket, args);
           });
         });
+      };
+
+      /** Options for onHandler:
+          {
+            callback: {
+              eventName: string,
+              callback: function,
+              offEvents: string[]
+            }
+          }
+          */
+
+      this.onHandler = options => {
+        options = Object(options);
+
+        let handler = (eventName, callback, offEvents) => {
+          this.on(eventName, data => {
+            this.off(eventName);
+            if (Array.isArray(offEvents)) {
+              offEvents.forEach(eventName => {
+                this.off(eventName);
+              });
+            }
+            if (callback) {
+              callback(data);
+            }
+          });
+        };
+
+        if (options.eventName) {
+          handler(options.eventName, options.callback, options.offEvents);
+        }
+      };
+
+      /** Options for emitHandler:
+          {
+            emit: {
+              event: string,
+              data: object,
+              success: "",
+              failure: ""
+            },
+            events: [
+              {
+                eventName: "",
+                method: "",
+                callback: callback,
+                offEvents: []
+              }
+            ]
+          }
+          */
+
+      this.emitHandler = options => {
+        options = Object(options);
+        let {
+          emit,
+          events
+        } = options;
+        this.emit(emit.eventName, emit.data);
+        if (Array.isArray(events)) {
+          events.forEach(event => {
+            this.onHandler(event.eventName, event.callback, event.offEvents);
+          });
+        }
       };
 
       this.disconnect = () => socket.disconnect();
