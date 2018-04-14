a5991d06960f6762bce46abd3720892aed6900a6
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-14 15:03:45 +0200

    Name changed. Module.exports added. New service included.

---
+++ dev/app/authorization/authorization.module.exports.js
@@ -0,0 +1,15 @@
+/* jshint esversion: 6 */
+import * as mod from "./authorization.module";
+import "./authorization.config";
+import "./authorization.service";
+import "./authorization.errorhandler";
+import "./authorization.socket";
+import "./authorization.userdata";
+import "./authorization.login";
+/** Constants */
+import "./constants/authorization.globals";
+import "./constants/authorization.states";
+import "./constants/authorization.events";
+
+
+module.exports = mod;
