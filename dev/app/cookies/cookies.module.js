ef0d79eaa476e7ff4057332beac24aff07fcb6a2
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-15 13:36:52 +0200

    cookiesModule included

--- dev/app/cookies/cookies.module.js
+++ dev/app/cookies/cookies.module.js
@@ -1,2 +1,7 @@
 /* jshint esversion: 6 */
-angular.module("cookiesModule", []);
+// import angular from "angular";
+import ngCookies from "angular-cookies";
+
+const mod = angular.module("cookiesModule", [ngCookies]);
+
+module.exports = mod;
