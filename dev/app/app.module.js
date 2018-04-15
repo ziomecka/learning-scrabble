4343e031252182955d9684791840f6cc919e7600
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-15 18:47:11 +0200

    ngAnimate, ngCookeis, ngRoute, uiRouter, newuser moved tpo other modules

--- dev/app/app.module.js
+++ dev/app/app.module.js
@@ -1,32 +1,17 @@
 /* jshint esversion: 6 */
 import angular from "angular";
-import ngRoute from "angular-route";
-import uiRouter from "@uirouter/angularjs";
-import ngCookies from "angular-cookies";
-import ngAnimate from "angular-animate";
 
-const domain = "http://localhost:5000";
+import authorizationModule from "./authorization/authorization.module.exports";
+import roomsModule from "./rooms/rooms.module.exports";
+import roomModule from "./room/room.module.exports";
 
-angular.module("app", [
-  ngRoute,
-  uiRouter,
-  ngCookies,
-  ngAnimate,
+import oclazyload from "oclazyload";
+
+const mod = angular.module("app", [
+  "oc.lazyLoad",
   "authorizationModule",
-  "newuserModule",
   "roomsModule",
   "roomModule"
  ]);
 
-import authorizationModule from "./authorization/authorization";
-import newuserModule from "./newuser/newuser";
-import roomsModule from "./rooms/rooms";
-import roomModule from "./room/room";
-
-angular.module("app")
-// .run(["$trace", function($trace) {$trace.enable("TRANSITION");}])
-.value("route", domain);
-
-// .factory("_", ["$window", lodashFactory])
-// import has from "lodash/has";
-// import lodashFactory from "./core/lodash.factory";
+module.exports = mod;
