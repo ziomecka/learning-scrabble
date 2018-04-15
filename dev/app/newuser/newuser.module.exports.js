e56626368b51b456a0003f1206147edbd5e7275a
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-15 14:00:29 +0200

    newuserjs renamed to newuser.module.exports. Module.exports added

---
+++ dev/app/newuser/newuser.module.exports.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+import * as mod from  "./newuser.module";
+import "./newuser.socket";
+import "./compare.passwords.directive";
+/** Constants */
+import "./constants/newuser.events";
+
+module.exports = mod;
