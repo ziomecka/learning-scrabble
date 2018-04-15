45faa68c3d94df917bf1f8b0755360080963c079
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-15 13:37:18 +0200

    name changed from cookies.js to cookies.module.exports

---
+++ dev/app/cookies/cookies.module.exports.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+import * as mod from "./cookies.module";
+import "./cookies.service";
+/** Constants */
+import "./constants/cookies.name";
+import "./constants/cookies.events";
+
+module.exports = mod;
