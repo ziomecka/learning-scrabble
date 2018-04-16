3e1ce919a932bc8d809230dbf5435381ec0a3acb
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-16 09:15:54 +0200

    Formatting

--- dev/app/router/services/router.lazyload.service.js
+++ dev/app/router/services/router.lazyload.service.js
@@ -6,28 +6,30 @@ angular
       "ngInject";
       this.load = mod => {
         return $q((resolve) => {
-          /** I use require.context insted of require.ensure
-          because of 'error: require is undefined',
-          which is supposed to be due to es6 syntax 'exports default'.
-          */
+          /** Require.context insted of require.ensure
+              because of 'error: require is undefined',
+              which is supposed to be due to es6 syntax 'exports default'.
+              */
           let contexts = {
             newuser: {
-                mod: () => require.context("../../newuser/", true, /newuser.module.exports.js*$/),
-                files:  () => require.context("../../newuser/", true, /^(?!.*\bexports\b).+js$/)
+                mod: () => require.context("../../newuser/", true, /newuser.module.exports.js$/),
+                files: () => require.context("../../newuser/", true, /^(?!.*\bexports\b).+js$/)
             },
             authorization: {
-                mod: () => require.context("../../authorization/", true, /authorization.module.exports.js*$/),
-                files:  () => require.context("../../authorization/", true, /^(?!.*\bexports\b).+js$/)
+                mod: () => require.context("../../authorization/", true, /authorization.module.exports.js$/),
+                files: () => require.context("../../authorization/", true, /^(?!.*\bexports\b).+js$/)
             }
           };
 
-          /** Load module. */
+          /** Require module. */
           let moduleContext = contexts[mod].mod();
           let module = moduleContext(moduleContext.keys()[0]);
 
-          /** Load remaining files. Files with 'exports' excluded. */
+          /** Require remaining files. Files with 'exports' excluded. */
           let filesContext = contexts[mod].files();
           filesContext.keys().forEach(key => filesContext(key));
+
+          /** Load module */
           $ocLazyLoad.load({name: `${mod}Module`});
 
           resolve(module);
