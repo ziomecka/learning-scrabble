d56ca2e5676765bfe44a9dfe16d2b0c305f7d626
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-16 09:15:24 +0200

    authorizationModule loaded wheile routing to  authorization state

--- dev/app/router/router.config.js
+++ dev/app/router/router.config.js
@@ -2,100 +2,97 @@
 // TODO what if url address is incorrect
 angular
   .module("routerModule")
-  .config([
-    "$routeProvider",
-    "$locationProvider",
-    "$stateProvider",
-    "routerStates",
-    ($routeProvider, $locationProvider, $stateProvider, routerStates) => {
-      const home = {
-        url: "/",
-        controller: require("../app.controller"),
-        resolve: {
-          loadAuthorizationModule: require("./resolve/resolve.load.authorization.module"),
-          authorizeCookie: require("./resolve/resolve.home.authorizecookie")
+  .config(($routeProvider, $locationProvider, $stateProvider, routerStates) => {
+    "ngInject";
+
+    const home = {
+      url: "/",
+      controller: require("../app.controller"),
+      resolve: {
+        authorizeCookie: require("./resolve/resolve.home.authorizecookie")
+      },
+    };
+
+    const authorization = {
+      url: "/authorization",
+      templateUrl: "../authorization.html",
+      controller: require("../authorization/controllers/authorization.controller"),
+      controllerAs: "authorizationCtrl",
+      resolve: {
+        loadAuthorizationModule: require("./resolve/resolve.load.authorization.module"),
+        fromApp: require("./resolve/resolve.fromhome")
+      }
+    };
+
+    const newuser = {
+      url: "/newuser",
+      templateUrl: "../newuser.html",
+      controller: require("../newuser/newuser.controller"),
+      resolve: {
+        loadNewUserModule: require("./resolve/resolve.load.newuser.module")
+      }
+    };
+
+    const games = {
+      abstract: true,
+      template: "<ui-view/>",
+      resolve: {
+        authorizeCookie: require("./resolve/resolve.home.authorizecookie")
+        // user: require("./resolve/resolve.games.user"), // TODO out because done in cookies?
+      }
+    };
+
+    const gamesRooms = {
+      url: "/rooms",
+      resolve: {
+        roomsList: require("./resolve/resolve.rooms.roomslist"),
+      },
+      views: {
+        "": {
+          templateUrl: "../rooms.html",
+          controller: require("../rooms/rooms.controller")
         },
-      };
-
-      const authorization = {
-        url: "/authorization",
-        templateUrl: "../authorization.html",
-        controller: require("../authorization/controllers/authorization.controller"),
-        controllerAs: "authorizationCtrl",
-        resolve: {
-          fromApp: require("./resolve/resolve.fromhome")
-        }
-      };
-
-      const newuser = {
-        url: "/newuser",
-        templateUrl: "../newuser.html",
-        controller: require("../newuser/newuser.controller"),
-        resolve: {
-          loadNewUserModule: require("./resolve/resolve.load.newuser.module")
-        }
-      };
-
-      const games = {
-        abstract: true,
-        template: "<ui-view/>",
-        resolve: {
-          authorizeCookie: require("./resolve/resolve.home.authorizecookie")
-          // user: require("./resolve/resolve.games.user"), // TODO out because done in cookies?
-        }
-      };
-
-      const gamesRooms = {
-        url: "/rooms",
-        resolve: {
-          roomsList: require("./resolve/resolve.rooms.roomslist"),
+        [`navigation@${routerStates.rooms}`]: {
+          templateUrl: "../rooms.navigation.html",
+          controller: require("../navigation/navigation.controller")
         },
-        views: {
-          "": {
-            templateUrl: "../rooms.html",
-            controller: require("../rooms/rooms.controller")
-          },
-          [`navigation@${routerStates.rooms}`]: {
-            templateUrl: "../rooms.navigation.html",
-            controller: require("../navigation/navigation.controller")
-          },
-          [`newroom@${routerStates.rooms}`]: {
-            templateUrl: "../rooms.newroom.html",
-            controller: require("../rooms/newroom/newroom.controller")
-          }
+        [`newroom@${routerStates.rooms}`]: {
+          templateUrl: "../rooms.newroom.html",
+          controller: require("../rooms/newroom/newroom.controller")
         }
-      };
+      }
+    };
 
-      const gamesRoom = {
-        url: "/rooms/room/:roomId",
-        resolve: {
-          roomData: require("./resolve/resolve.room.roomdata"),
-          narrowTitle: require("./resolve/resolve.room.narrowtitle")
+    const gamesRoom = {
+      url: "/rooms/room/:roomId",
+      resolve: {
+        roomData: require("./resolve/resolve.room.roomdata"),
+        narrowTitle: require("./resolve/resolve.room.narrowtitle")
+      },
+      params: {
+        newroom: false
+      },
+      views: {
+        "": {
+          templateUrl: "../room.html",
+          controller: require("../room/room.controller")
         },
-        params: {
-          newroom: false
-        },
-        views: {
-          "": {
-            templateUrl: "../room.html",
-            controller: require("../room/room.controller")
-          },
-          [`roomsidebar@${routerStates.room}`]: {
-            templateUrl: "../room.sidebar.html",
-            controller: require("../room/sidebar/room.sidebar.controller")
-          }
+        [`roomsidebar@${routerStates.room}`]: {
+          templateUrl: "../room.sidebar.html",
+          controller: require("../room/sidebar/room.sidebar.controller")
         }
-      };
+      }
+    };
 
-      $routeProvider.otherwise({templateUrl: "../index.html"});
+    $routeProvider.otherwise({templateUrl: "../index.html"});
 
-      $stateProvider.state(routerStates.home, home);
-      $stateProvider.state(routerStates.authorization, authorization);
-      $stateProvider.state(routerStates.newuser, newuser);
-      $stateProvider.state(routerStates.games, games);
-      $stateProvider.state(routerStates.rooms, gamesRooms);
-      $stateProvider.state(routerStates.room, gamesRoom);
+    $stateProvider.state(routerStates.home, home);
+    $stateProvider.state(routerStates.authorization, authorization);
+    $stateProvider.state(routerStates.newuser, newuser);
+    $stateProvider.state(routerStates.games, games);
+    $stateProvider.state(routerStates.rooms, gamesRooms);
+    $stateProvider.state(routerStates.room, gamesRoom);
 
-      $locationProvider.html5Mode({enabled: true});
-    }
-  ]);
+    $locationProvider.html5Mode({enabled: true});
+  }
+);
