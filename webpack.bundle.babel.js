d03b3312092888926a1262ba30c0945d3c646bf1
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-17 14:47:20 +0200

    Scrabble pugs included, plugins grouped in one object

--- webpack.bundle.babel.js
+++ webpack.bundle.babel.js
@@ -9,93 +9,104 @@ import CopyWebpackPlugin from "copy-webpack-plugin";
 import path from "path";
 import webpack from "webpack";
 
-const htmlIndex = {
-  title: "Let's play scrabble",
-  template: path.join("./app/index.pug"),
-  chunks: ["app"],
-  filename: "./index.html",
-  inject: "head",
-  // meta: "", // TODO
-  // favicon: "",
-  // minify: ""
-};
-
 const htmlIndexAssets = {
   assets: [path.join("./app/css/index.css")],
   append: true
 };
-
-const htmlTitle = {
-  template: path.join("./app/home/templates/title.pug"),
-  filename: "./index.title.html",
-  inject: false
+const htmlsOptions = {
+  htmlIndex: {
+    title: "Let's play scrabble",
+    template: path.join("./app/index.pug"),
+    chunks: ["app"],
+    filename: "./index.html",
+    inject: "head",
+    // meta: "", // TODO
+    // favicon: "",
+    // minify: ""
+  },
+  htmlTitle: {
+    template: path.join("./app/home/templates/title.pug"),
+    filename: "./index.title.html",
+    inject: false
+  },
+  htmlNavigation: {
+    template: path.join("./app/navigation/navigation.pug"),
+    filename: "./rooms.navigation.html",
+    inject: false
+  },
+  htmlRoomSidebar: {
+    template: path.join("./app/room/sidebar/room.sidebar.pug"),
+    filename: "./room.sidebar.html",
+    inject: false
+  },
+  htmlRoomControls: {
+    template: path.join("./app/room/controls/controls.pug"),
+    filename: "./room.controls.html",
+    inject: false
+  },
+  htmlRooms: {
+    template: path.join("./app/rooms/rooms.pug"),
+    filename: "./rooms.html",
+    inject: false
+  },
+  htmlAuth: {
+    template: path.join("./app/authorization/templates/authorization.pug"),
+    filename: "./authorization.html",
+    inject: false
+  },
+  htmlNewuser: {
+    template: path.join("./app/newuser/newuser.pug"),
+    filename: "./newuser.html",
+    inject: false
+  },
+  htmlNewroom: {
+    template: path.join("./app/rooms/newroom/newroom.pug"),
+    filename: "./rooms.newroom.html",
+    inject: false
+  },
+  htmlRoom: {
+    template: path.join("./app/room/room.pug"),
+    filename: "./room.html",
+    inject: false
+  },
+  htmlControlsRound: {
+    template: path.join("./app/scrabble/controls/round/round.pug"),
+    filename: "./room.controls.round.html",
+    inject: false
+  },
+  htmlControlsExchange: {
+    template: path.join("./app/scrabble/controls/exchange/exchange.pug"),
+    filename: "./room.controls.exchange.html",
+    inject: false
+  },
+  htmlControlsVerify: {
+    template: path.join("./app/scrabble/controls/verify/verify.pug"),
+    filename: "./room.controls.verify.html",
+    inject: false
+  },
+  htmlScrabble: {
+    template: path.join("./app/scrabble/scrabble.pug"),
+    filename: "./scrabble.html",
+    inject: false
+  },
+  htmlBoard: {
+    template: path.join("./app/scrabble/board/templates/board.pug"),
+    filename: "./scrabble.board.html",
+    inject: false
+  },
+  htmlField: {
+    template: path.join("./app/scrabble/board/templates/field.pug"),
+    filename: "./scrabble.field.html",
+    inject: false
+  }
 };
 
-const htmlNavigation = {
-  template: path.join("./app/navigation/navigation.pug"),
-  filename: "./rooms.navigation.html",
-  inject: false
-};
+const htmlPlugins = [];
 
-const htmlRoomSidebar = {
-  template: path.join("./app/room/sidebar/room.sidebar.pug"),
-  filename: "./room.sidebar.html",
-  inject: false
-};
-
-const htmlRoomControls = {
-  template: path.join("./app/room/controls/controls.pug"),
-  filename: "./room.controls.html",
-  inject: false
-};
-
-const htmlRooms = {
-  template: path.join("./app/rooms/rooms.pug"),
-  filename: "./rooms.html",
-  inject: false
-};
-
-const htmlAuth = {
-  template: path.join("./app/authorization/templates/authorization.pug"),
-  filename: "./authorization.html",
-  inject: false
-};
-
-const htmlNewuser = {
-  template: path.join("./app/newuser/newuser.pug"),
-  filename: "./newuser.html",
-  inject: false
-};
-
-const htmlNewroom = {
-  template: path.join("./app/rooms/newroom/newroom.pug"),
-  filename: "./rooms.newroom.html",
-  inject: false
-};
-
-const htmlRoom = {
-  template: path.join("./app/room/room.pug"),
-  filename: "./room.html",
-  inject: false
-};
-
-const htmlControlsRound = {
-  template: path.join("./app/scrabble/controls/round/round.pug"),
-  filename: "./room.controls.round.html",
-  inject: false
-};
-
-const htmlControlsExchange = {
-  template: path.join("./app/scrabble/controls/exchange/exchange.pug"),
-  filename: "./room.controls.exchange.html",
-  inject: false
-};
-
-const htmlControlsVerify = {
-  template: path.join("./app/scrabble/controls/verify/verify.pug"),
-  filename: "./room.controls.verify.html",
-  inject: false
-};
+Object.keys(htmlsOptions)
+  .forEach(htmlOption => {
+    htmlPlugins.push(new HtmlWebpackPlugin(htmlsOptions[htmlOption]));
+  });
 
 const copyOptions = [
   {
@@ -114,19 +125,7 @@ const settings = merge (common, {
     filename: "./app/[name].bundle.js"
   },
   plugins: [
-    new HtmlWebpackPlugin(htmlIndex),
-    new HtmlWebpackPlugin(htmlTitle),
-    new HtmlWebpackPlugin(htmlNavigation),
-    new HtmlWebpackPlugin(htmlRoom),
-    new HtmlWebpackPlugin(htmlRoomSidebar),
-    new HtmlWebpackPlugin(htmlRooms),
-    new HtmlWebpackPlugin(htmlAuth),
-    new HtmlWebpackPlugin(htmlNewuser),
-    new HtmlWebpackPlugin(htmlNewroom),
-    new HtmlWebpackPlugin(htmlRoomControls),
-    new HtmlWebpackPlugin(htmlControlsRound),
-    new HtmlWebpackPlugin(htmlControlsExchange),
-    new HtmlWebpackPlugin(htmlControlsVerify),
+    ...htmlPlugins,
     new HtmlWebpackIncludeAssetsPlugin(htmlIndexAssets),
     new CopyWebpackPlugin(copyOptions)
   ]
