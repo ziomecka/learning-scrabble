ab911380f9d95d271c33ad22d635dc9f95300295
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-17 12:28:05 +0200

    tileFactory as a function, ng-annotate used

--- dev/app/scrabble/tile/tile.factory.js
+++ dev/app/scrabble/tile/tile.factory.js
@@ -1,66 +1,15 @@
 /* jshint esversion: 6 */
-angular
-.module("tileModule")
-.factory("tileFactory", [
-  "tileOptions",
-  "tileStates",
-  function (tileOptions, tileStates) {
-    class Tile {
-      constructor() {
-        ({letter: this.letter, points: this.points} = tileOptions);
-        // this._state = tileStates.get("inBag");
-      }
+const tileFactory = (tileOptions, tileStates) => {
+  "ngInject";
 
-      // get onRack ()  {
-      //   return this._state === tileStates.get("player").get("rack");
-      // }
-      //
-      // set onRack (bool) {
-      //   if (bool === true) {
-      //     this._state = tileStates.get("player").get("rack");
-      //   }
-      // }
-      //
-      // get toBeExchanged () {
-      //   return this._state === tileStates.get("player").get("exchanged");
-      // }
-      //
-      // set toBeExchanged (bool) {
-      //   if (bool === true) {
-      //     this._state = tileStates.get("player").get("exchanged");
-      //   }
-      // }
-      //
-      // get isPlayed () {
-      //   return this._state === tileStates.get("player").get("played");
-      // }
-      //
-      // set isPlayed (bool) {
-      //   if (bool === true) {
-      //     this._state = tileStates.get("player").get("played");
-      //   }
-      // }
-      //
-      // get onBoard () {
-      //   return this._state === tileStates.get("board");
-      // }
-      //
-      // set onBoard (bool) {
-      //   if (bool === true) {
-      //     this._state = tileStates.get("board");
-      //   }
-      // }
-      //
-      // get inBag () {
-      //   return this._state === tileStates.get("bag");
-      // }
-      //
-      // set inBag (bool) {
-      //   if (bool === true) {
-      //     this._state = tileStates.get("bag");
-      //   }
-      // }
+  class Tile {
+    constructor() {
+      ({letter: this.letter, points: this.points} = tileOptions);
     }
-    return Tile;
   }
-]);
+  return Tile;
+};
+
+angular
+  .module("tileModule")
+  .factory("tileFactory", tileFactory);
