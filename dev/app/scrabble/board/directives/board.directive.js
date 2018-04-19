ae3fb594cc6082711d5a40518c937def35a91a16
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-19 11:09:15 +0200

    Changes in comments

--- dev/app/scrabble/board/directives/board.directive.js
+++ dev/app/scrabble/board/directives/board.directive.js
@@ -2,9 +2,6 @@
 const boardDirective = () => {
   "ngInject";
   return {
-    // scope: {
-    //   boardInfo: "=board"
-    // },
     restrict: "E",
     templateUrl: "scrabble.board.html",
     controller: require("../board.controller"),
