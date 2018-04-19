cc48b2b89ae0f0014fa5a243c28dd91a72123303
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-19 11:13:15 +0200

    Propoerties droppable and setter and getter for _tile

--- dev/app/scrabble/field/field.factory.js
+++ dev/app/scrabble/field/field.factory.js
@@ -1,11 +1,29 @@
 /* jshint esversion: 6 */
 const fieldFactory = () => {
+  "ngInject";
   class Field {
     constructor(options) {
       ({row: this.row, column: this.column, drawOptions: this.drawOptions} = options);
       this.row = String(this.row);
-      this.tile = null;
+      this._tile = null;
       this.bonus = "none";
+      this._droppable = true;
+      this.txt = "dupa";
+    }
+
+    get droppable () {
+      return this._droppable;
+    }
+
+    set tile (tile) {
+      this._tile = tile;
+      this._droppable = tile? false : true;
+    }
+    get tile () {
+      return this._tile;
+    }
+    set droppable (boolean) {
+      this._droppable = boolean;
     }
 
     put (tile) {
