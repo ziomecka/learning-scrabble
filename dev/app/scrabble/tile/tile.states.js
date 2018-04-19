08e395e8aa62cae7eb396a815b31eecb758dc6ad
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-19 11:19:02 +0200

    Rewritten to ES6.

--- dev/app/scrabble/tile/tile.states.js
+++ dev/app/scrabble/tile/tile.states.js
@@ -1,12 +1,16 @@
 /* jshint esversion: 6 */
+const tileStates = () => {
+  return new Map([
+    ["board", Symbol("board")],
+    ["bag", Symbol("bag")],
+    ["player", new Map([
+      ["rack", Symbol("rack")],
+      ["exchanged", Symbol("exchanged")],
+      ["played", Symbol("played")]
+    ])]
+  ]);
+};
+
 angular
-.module("tileModule")
-.constant("tileStates", new Map([
-  ["board", Symbol("board")],
-  ["bag", Symbol("bag")],
-  ["player", new Map([
-    ["rack", Symbol("rack")],
-    ["exchanged", Symbol("exchanged")],
-    ["played", Symbol("played")]
-  ])]
-]));
+  .module("tileModule")
+  .constant("tileStates", tileStates);
