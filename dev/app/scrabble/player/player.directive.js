33fa0e956db8ef02314d2701dd28c20b799773ac
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-19 11:21:22 +0200

    Templaeurl, link function, controller included.

--- dev/app/scrabble/player/player.directive.js
+++ dev/app/scrabble/player/player.directive.js
@@ -1,18 +1,16 @@
 /* jshint esversion: 6 */
+const scrabblePlayerDirective = () => {
+  const link = (scope, element, attrs) => {
+  };
+
+  return {
+    templateUrl: "../scrabble.player.html",
+    link: link,
+    controller: require("./player.controller"),
+    controllerAs: "playerCtrl"
+  };
+};
+
 angular
   .module("playerModule")
-  .directive("playerDirective", [
-    (players) => {
-      let template = "";
-      players.forEach((player) => {
-        template += `
-        <p ng-bind="{{player.points}}"></p>
-        <p ng-bind="{{player.time}}"></p>
-        `;
-      });
-
-      return {
-        template: template
-      };
-    }
-  ]);
+  .directive("scrabblePlayer", scrabblePlayerDirective);
