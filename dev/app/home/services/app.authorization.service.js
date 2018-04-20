f0e4c4d278dde8a51c7416a49ff2d0757d3063be
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 17:21:22 +0200

    Rewritten to ES6.

--- dev/app/home/services/app.authorization.service.js
+++ dev/app/home/services/app.authorization.service.js
@@ -1,32 +1,49 @@
 /* jshint esversion: 6 */
-angular
-.module("app")
-.service("authorizationService",
-  function ($state, routerStates, cookiesService, userData, routerGoService) {
+class AuthorizationService {
+  constructor (
+    $state,
+    routerStates,
+    cookiesService,
+    userData,
+    routerGoService
+  ) {
     "ngInject";
-    this.clear = () => {
-      userData.authorized = false;
-      $state.go(routerStates.authorization);
-    };
 
-    /** Store login in authorization service. */
-    this.authorize = data => {
-      ({
-        authorized: userData.authorized = true,
-        login:  userData.login = ""
-      } = data);
-      routerGoService.go();
-    };
-
-    this.unauthorize = () => {
-      userData.authorized = false;
-      userData.login = "";
-      // TODO change name of the below service
-      /** Destroy cookies */
-      cookiesService.removeAuthorizationCookies();
-      // TODO off all or some sockets???
-      /** Go back to authorization state */
-      routerGoService.go({state: routerStates.authorization});
-    };
+    Object.assign(this, {
+      $state,
+      routerStates,
+      cookiesService,
+      userData,
+      routerGoService
+    });
   }
-);
+
+  clear () {
+    this.userData.authorized = false;
+    this.$state.go(this.routerStates.authorization);
+  }
+
+  /** Store login in authorization service. */
+  authorize (data) {
+    ({
+      authorized: this.userData.authorized = true,
+      login: this.userData.login = ""
+    } = data);
+    this.routerGoService.go();
+  }
+
+  unauthorize () {
+    this.userData.authorized = false;
+    this.userData.login = "";
+    // TODO change name of the below service
+    /** Destroy cookies */
+    this.cookiesService.removeAuthorizationCookies();
+    // TODO off all or some sockets???
+    /** Go back to authorization state */
+    this.routerGoService.go({state: this.routerStates.authorization});
+  }
+}
+
+angular
+  .module("app")
+  .service("authorizationService", AuthorizationService);
