4fd3390819fb1648630724e5252635bd77121d81
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 17:12:03 +0200

    ngInject added

--- dev/app/router/resolve/resolve.room.narrowtitle.js
+++ dev/app/router/resolve/resolve.room.narrowtitle.js
@@ -1,8 +1,6 @@
 /* jshint esversion: 6 */
 // TODO think of other way of doing this
-module.exports = [
-  "$rootScope",
-  ($rootScope) => {
-    $rootScope.narrowTitle = true;
-  }
-];
+module.exports = $rootScope => {
+  "ngInject";
+  $rootScope.narrowTitle = true;
+};
