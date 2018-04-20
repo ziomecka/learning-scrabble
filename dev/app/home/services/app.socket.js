3dcb3968f2b2afc06fe6d2f90367e77ce9f7b1da
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 17:18:06 +0200

    Rewritten to ES6

--- dev/app/home/services/app.socket.js
+++ dev/app/home/services/app.socket.js
@@ -1,21 +1,33 @@
 /* jshint esversion: 6 */
+class AppSocket {
+  constructor (
+    socketService,
+    appEvents
+  ) {
+    "ngInject";
+
+    Object.assign(this, {
+      socketService,
+      appEvents
+    });
+  }
+
+  connection () {
+    this.socketService.on(this.appEvents.resConnected, data => console.log("I'm connected"));
+    this.socketService.on("disconnect", () => console.log("I have been disconnected"));
+  }
+
+  removeListeners () {
+    this.socketService.getSocket().removeAllListeners();
+  }
+
+  getAllRooms (options) {
+    let {callback} = options;
+    this.socketService.emit(this.appEvents.reqAllRooms);
+    this.socketService.on(this.appEvents.resAllRooms, data => callback(data));
+  }
+}
+
 angular
   .module("app")
-  .service("appSocket", function (socketService, appEvents) {
-    "ngInject";
-    this.connection = () => {
-      socketService.on(appEvents.resConnected, data => console.log("I'm connected"));
-      socketService.on("disconnect", () => console.log("I have been disconnected"));
-    };
-
-    this.removeListeners = () => {
-      socketService.getSocket().removeAllListeners();
-    };
-
-    this.getAllRooms = options => {
-      let {callback} = options;
-      socketService.emit(appEvents.reqAllRooms);
-      socketService.on(appEvents.resAllRooms, data => callback(data));
-    };
-  }
-);
+  .service("appSocket", AppSocket);
