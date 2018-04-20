e6e06783676e5befa843b81d2f9372d871b99fbc
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 17:09:11 +0200

    ngInject added

--- dev/app/router/resolve/resolve.games.user.js
+++ dev/app/router/resolve/resolve.games.user.js
@@ -2,16 +2,16 @@
 /** Check if user is authorized.
     TODO can be removed?
     */
-module.exports = [
-  "userData",
-  "$q",
-  (userData, $q) => {
-    const deferred = $q.defer();
-    if (userData.authorized) {
-      deferred.resolve(userData.login); // TODO correct?
-    } else {
-      deferred.reject("not authorized");
-    }
-    return deferred.promise;
+module.exports = (
+  userData,
+  $q
+) => {
+  "ngInject";
+  const deferred = $q.defer();
+  if (userData.authorized) {
+    deferred.resolve(userData.login); // TODO correct?
+  } else {
+    deferred.reject("not authorized");
   }
-];
+  return deferred.promise;
+};
