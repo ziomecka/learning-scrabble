14b1b9610b987ef4196c2feb38bb0ad81147488d
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 14:47:08 +0200

    Tilefactory included. Size, bonuses, and fieldFactory assigned to instance. Fields property replaced with rows property. PlaceTile method added.

--- dev/app/scrabble/board/board.service.js
+++ dev/app/scrabble/board/board.service.js
@@ -1,15 +1,23 @@
 /* jshint esversion: 6 */
 class Board {
-  constructor(fieldFactory, boardOptions) {
+  constructor(
+    fieldFactory,
+    tileFactory,
+    boardOptions
+  ) {
     "ngInject";
-    let {size: size, bonuses: bonuses} = boardOptions;
+
+    ({size: this.size, bonuses: this.bonuses} = boardOptions);
 
     this.directions = ["row", "column"];
     this.side = [-1, 1];
 
+    Object.assign(this, {
+      fieldFactory,
+      tileFactory
+    });
 
-
-    this.fields = this.createFields();
+    this.rows = this.createFields();
     this.setBonuses();
   }
 
@@ -17,9 +25,9 @@ class Board {
     let row = field.row;
     let column = field.column;
     if (direction === "column") {
-      return (row + side >= 0)? this.fields[row + side][column] : null;
+      return (row + side >= 0)? this.rows[row + side][column] : null;
     } else if (direction === "row") {
-      return (column + side >= 0)? this.fields[row][column - 1] : null;
+      return (column + side >= 0)? this.rows[row][column - 1] : null;
     } else {
       throw Error("");
     }
@@ -31,24 +39,38 @@ class Board {
 
   createFields () {
     let result = [];
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < this.size; i++) {
       let row = result[i] = [];
-      for (let j = 0; j < size; j++) {
-        row[j] = new fieldFactory({"row": i, "column": j});
+      for (let j = 0; j < this.size; j++) {
+        row[j] = new this.fieldFactory({"row": i, "column": j});
       }
     }
+
+    result[0][0].tile = new this.tileFactory({letter: "A", "points": 1});
+    result[0][1].tile = new this.tileFactory({letter: "B", "points": 2});
     return result;
   }
 
   setBonuses() {
-    Object.keys(bonuses).forEach(type => {
-      bonuses[type].forEach(bonus => {
+    Object.keys(this.bonuses).forEach(type => {
+      this.bonuses[type].forEach(bonus => {
         let row = bonus[0] - 1;
         let column = bonus[1] - 1;
-        this.fields[row][column].bonus = type;
+        this.rows[row][column].bonus = type;
       });
     });
   }
+
+  placeTile(options) {
+    let {tile, field} = options;
+    if (tile) {
+      this.rows[field.column][field.row].tile = tile;
+      tile = null;
+      field = null;
+    }
+  }
 }
 
-angular.module("boardModule", Board);
+angular
+  .module("boardModule")
+  .service("boardService", Board);
