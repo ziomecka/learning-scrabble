f20b6fc3d77fb93df7cd7af1810bfca301487823
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 17:08:31 +0200

    ngInject added.

--- dev/app/router/resolve/resolve.fromhome.js
+++ dev/app/router/resolve/resolve.fromhome.js
@@ -1,23 +1,23 @@
 /* jshint esversion: 6 */
-module.exports = [
-  "$transition$",
-  "$q",
-  "$timeout",
-  "appGlobals",
-  ($transition$, $q, $timeout, appGlobals) => {
-    const deferred = $q.defer();
-    /** If the previous state was 'home' then
-        ui-router resolves the 'authorization' state with 3s delay.
-        In this time the title 'scrabble' is made shorter
-        (by addding '--short' class to the title).
-        */
-    if ($transition$.from().name === "home") {
-      $timeout(() => {
-        deferred.resolve();
-      }, appGlobals.delayedViewTime);
-    } else {
-        deferred.resolve();
-    }
-    return deferred.promise;
+module.exports = (
+  $transition$,
+  $q,
+  $timeout,
+  appGlobals
+) => {
+  "ngInject";
+  const deferred = $q.defer();
+  /** If the previous state was 'home' then
+  ui-router resolves the 'authorization' state with 3s delay.
+  In this time the title 'scrabble' is made shorter
+  (by addding '--short' class to the title).
+  */
+  if ($transition$.from().name === "home") {
+    $timeout(() => {
+      deferred.resolve();
+    }, appGlobals.delayedViewTime);
+  } else {
+    deferred.resolve();
   }
-];
+  return deferred.promise;
+};
