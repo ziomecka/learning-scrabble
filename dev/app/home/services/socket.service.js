40f82c5f9b8e4bd249fec37b10c35e9fd58cc018
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 22:05:45 +0200

    Revert "rewritten to ES6."
    This reverts commit 6eb9cb609dfe3e1c834aa74bc037254faf96b9b7.

--- dev/app/home/services/socket.service.js
+++ dev/app/home/services/socket.service.js
@@ -1,115 +1,102 @@
 /* jshint esversion: 6 */
-class SocketService {
-  constructor (
-    $rootScope,
-    userData
-  ) {
-    "ngInject";
-
-    Object.assign(this, {
-      $rootScope,
-      userData
-    });
-  }
-
-  /** io is avaialable thanks to socket.io-client */
-  get socket () {
-    return io();
-  }
-
-  on (event, callback) {
-    this.socket.on(event, function() {
-      let args = arguments;
-      this.$rootScope.$apply(() => {
-        callback.apply(socket, args);
-      });
-    });
-  }
-
-  off (event) {
-    this.socket.off(event);
-  }
-
-  emit (event, data, callback) {
-    this.socket.emit(event, data, () => {
-      const args = arguments;
-      this.$rootScope.$apply(() => {
-        callback.apply(socket, args);
-      });
-    });
-  }
-
-    /** Options for onHandler:
-        {
-          callback: {
-          eventName: string,
-          callback: function,
-          offEvents: string[]
-        }
-      }
-      */
-
-  onHandler (options) {
-    options = Object(options);
-
-    let handler = (eventName, callback, offEvents) => {
-      this.on(eventName, data => {
-        this.off(eventName);
-        if (Array.isArray(offEvents)) {
-          offEvents.forEach(eventName => {
-            this.off(eventName);
-          });
-        }
-        if (callback) {
-          callback(data);
-        }
-      });
-    };
-
-    if (options.eventName) {
-      handler(options.eventName, options.callback, options.offEvents);
-    }
-  }
-
-  /** Options for emitHandler:
-  {
-    emit: {
-      event: string,
-      data: object
-    },
-    events: [
-      {
-        eventName: string,
-        callback: function,
-        offEvents: string[]
-      }
-    ]
-  }
-  */
-
-  emitHandler (options) {
-    options = Object(options);
-    let {
-      emit,
-      events
-    } = options;
-
-    /** Emit roomID and login */
-    ({login: emit.data.login, roomId: emit.data.roomId} = userData);
-
-    this.emit(emit.eventName, emit.data);
-    if (Array.isArray(events)) {
-      events.forEach(event => {
-        this.onHandler(event);
-      });
-    }
-  }
-
-  disconnect () {
-    this.socket.disconnect();
-  }
-}
-
 angular
   .module("app")
-  .service("socketService", SocketService);
+  .service("socketService", function ($rootScope, userData) {
+      "ngInject";
+      /** io is avaialable thanks to socket.io-client */
+      let socket = io();
+
+      this.on = (event, callback) => {
+        socket.on(event, function() {
+          let args = arguments;
+          $rootScope.$apply(() => {
+            callback.apply(socket, args);
+          });
+        });
+      };
+
+      this.off = (event) => {
+        socket.off(event);
+      };
+
+
+      this.emit = (event, data, callback) => {
+        socket.emit(event, data, () => {
+          const args = arguments;
+          $rootScope.$apply(() => {
+            callback.apply(socket, args);
+          });
+        });
+      };
+
+      /** Options for onHandler:
+          {
+            callback: {
+              eventName: string,
+              callback: function,
+              offEvents: string[]
+            }
+          }
+          */
+
+      this.onHandler = options => {
+        options = Object(options);
+
+        let handler = (eventName, callback, offEvents) => {
+          this.on(eventName, data => {
+            this.off(eventName);
+            if (Array.isArray(offEvents)) {
+              offEvents.forEach(eventName => {
+                this.off(eventName);
+              });
+            }
+            if (callback) {
+              callback(data);
+            }
+          });
+        };
+
+        if (options.eventName) {
+          handler(options.eventName, options.callback, options.offEvents);
+        }
+      };
+
+      /** Options for emitHandler:
+          {
+            emit: {
+              event: string,
+              data: object
+            },
+            events: [
+              {
+                eventName: string,
+                callback: function,
+                offEvents: string[]
+              }
+            ]
+          }
+          */
+
+      this.emitHandler = options => {
+        options = Object(options);
+        let {
+          emit,
+          events
+        } = options;
+
+        /** Emit roomID and login */
+        ({login: emit.data.login, roomId: emit.data.roomId} = userData);
+
+        this.emit(emit.eventName, emit.data);
+        if (Array.isArray(events)) {
+          events.forEach(event => {
+            this.onHandler(event);
+          });
+        }
+      };
+
+      this.disconnect = () => socket.disconnect();
+
+      this.getSocket = () => this.socket;
+  }
+);
