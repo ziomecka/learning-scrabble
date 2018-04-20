3fe3b73bd9a66be2dcaeb73e6b37d951f38482b1
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 15:10:32 +0200

    Rewritten to ES6.

--- dev/app/rooms/rooms.socket.js
+++ dev/app/rooms/rooms.socket.js
@@ -1,73 +1,86 @@
 /* jshint esversion: 6 */
+class RoomsSocket {
+  constructor (
+    socketService,
+    roomsEvents,
+    $stateParams,
+    routerGoService,
+    userData
+  ) {
+    "ngInject";
+
+    Object.assign(this, {
+      socketService,
+      roomsEvents,
+      $stateParams,
+      routerGoService,
+      userData
+    });
+  }
+
+  getRoomDetails (options) {
+    let {callbacks: {success}} = options;
+    this.socketService.emit(this.roomsEvents.reqJoinedRoomDetails, {
+      roomId: options.roomId,
+      login: this.userData.login
+    });
+
+    this.listenGetRoomDetails({
+      callbacks: {
+        success: success
+      }
+    });
+  }
+
+  listenGetRoomDetails (options) {
+    let {callbacks: {success}} = options;
+    let eventName= this.roomsEvents.resJoinedRoomDetails;
+    this.socketService.on(eventName, data => {
+      success(data);
+      this.socketService.off(eventName);
+    });
+  }
+
+  createRoom (options) {
+    let {name, numberPlaces, createGame = true, joinRoom = true, callbacks: {success}} = options;
+    this.$stateParams.numberPlaces = numberPlaces;
+    this.socketService.emit(this.roomsEvents.reqCreateNewroom, {
+      name: name,
+      numberPlaces: numberPlaces,
+      login: this.userData.login,
+      createGame: createGame,
+      joinRoom: joinRoom
+    });
+    this.createRoomSuccess(success);
+  }
+
+  createRoomSuccess (callback) {
+    this.socketService.on(this.roomsEvents.resCreateNewroom, data => callback(data));
+  }
+
+  // this.newRoomAdded = options => {
+  //   let {callbacks: {success}} = options;
+  //   this.socketService.on(this.roomsEvents.resNewroomAdded, data => {
+  //     success(data);
+  //   });
+  // };
+
+  /** Join room and
+  listen to room details */
+  joinRoom (options) {
+    let {id, callbacks: {successJoinRoom}} = options;
+    this.socketService.emit(this.roomsEvents.reqJoinRoom, {"id": id});
+    this.socketService.on(this.roomsEvents.resRoomJoinedSuccess, data => {
+      successJoinRoom();
+      this.routerGoService.go({
+        state: auhtorizationStates.room,
+        roomId: data.roomId
+      });
+    });
+    // this.socketService.on (appEveents.resJoinedRoomDetails, data => {});
+  }
+}
+
 angular
   .module("roomsModule")
-  .service("roomsSocket", [
-    "socketService",
-    "roomsEvents",
-    "$stateParams",
-    "routerGoService",
-    "userData",
-    function (socketService, roomsEvents, $stateParams, routerGoService, userData) {
-      this.getRoomDetails = (options) => {
-        let {callbacks: {success}} = options;
-        socketService.emit(roomsEvents.reqJoinedRoomDetails, {
-          roomId: options.roomId,
-          login: userData.login
-        });
-
-        this.listenGetRoomDetails({
-          callbacks: {
-            success: success
-          }
-        });
-      };
-
-      this.listenGetRoomDetails = (options) => {
-        let {callbacks: {success}} = options;
-        let eventName= roomsEvents.resJoinedRoomDetails;
-        socketService.on(eventName, data => {
-          success(data);
-          socketService.off(eventName);
-        });
-      };
-
-      this.createRoom = options => {
-        let {name, numberPlaces, createGame = true, joinRoom = true, callbacks: {success}} = options;
-        $stateParams.numberPlaces = numberPlaces;
-        socketService.emit(roomsEvents.reqCreateNewroom, {
-          name: name,
-          numberPlaces: numberPlaces,
-          login: userData.login,
-          createGame: createGame,
-          joinRoom: joinRoom
-        });
-        this.createRoomSuccess(success);
-      };
-
-      this.createRoomSuccess = callback => {
-        socketService.on(roomsEvents.resCreateNewroom, data => callback(data));
-      };
-
-      // this.newRoomAdded = options => {
-      //   let {callbacks: {success}} = options;
-      //   socketService.on(roomsEvents.resNewroomAdded, data => {
-      //     success(data);
-      //   });
-      // };
-
-      /** Join room and
-          listen to room details */
-      this.joinRoom = options => {
-        let {id, callbacks: {successJoinRoom}} = options;
-        socketService.emit(roomsEvents.reqJoinRoom, {"id": id});
-        socketService.on(roomsEvents.resRoomJoinedSuccess, data => {
-          successJoinRoom();
-          routerGoService.go({
-            state: auhtorizationStates.room,
-            roomId: data.roomId
-          });
-        });
-        // socketService.on (appEveents.resJoinedRoomDetails, data => {});
-      };
-    }
-  ]);
+  .service("roomsSocket", RoomsSocket);
