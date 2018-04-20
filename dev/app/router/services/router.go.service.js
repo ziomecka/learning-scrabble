06d47c07cc1d4af31f04552e363c2b47d5a84722
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-20 17:00:44 +0200

    Rewritten to ES6.

--- dev/app/router/services/router.go.service.js
+++ dev/app/router/services/router.go.service.js
@@ -1,29 +1,39 @@
 /* jshint esversion: 6 */
+class RouterGoService {
+  constructor (
+    $state,
+    routerStates,
+    userData
+  ) {
+    "ngInject";
+
+    Object.assign(this, {
+      $state,
+      routerStates,
+      userData
+    });
+  }
+
+  // TODO some bullshit when trying to enter room that does not exist
+  go (data) {
+    /** Get data */
+    let {
+      state: state = this.userData.authorized? this.routerStates.rooms: this.routerStates.home,
+      roomId: roomId,
+      newroom: newroom = false // TODO remove?
+    } = Object(data);
+
+    /** Store roomId in user's data. */
+    this.userData.roomId = roomId;
+
+    /** Pass roomId and information if the room is new. */
+    this.$state.go(state, {
+      "roomId": roomId,
+      "newroom": newroom
+    });
+  }
+}
+
 angular
   .module("routerModule")
-  .service("routerGoService", [
-    "$state",
-    "routerStates",
-    "userData",
-    function ($state, routerStates, userData) {
-      // TODO some bullshit when trying to enter room that does not exist
-      /** Passes roomId and information if the room is new. */
-      this.go = data => {
-        data = Object(data);
-        /** Get data */
-        let {
-              state: state = userData.authorized? routerStates.rooms: routerStates.home,
-              roomId: roomId,
-              newroom: newroom = false
-            } = data;
-
-        /** Store roomId in user's data. */
-        userData.roomId = roomId;
-
-        $state.go(state, {
-          "roomId": roomId,
-          "newroom": newroom
-        });
-      };
-    }]
-  );
+  .service("routerGoService", RouterGoService);
