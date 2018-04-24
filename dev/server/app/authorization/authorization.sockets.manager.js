3e67eb68aa49273ff51a6488852dc9e4b31bec08
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-24 12:51:37 +0200

    listSockets method added.

--- dev/server/app/authorization/authorization.sockets.manager.js
+++ dev/server/app/authorization/authorization.sockets.manager.js
@@ -1,21 +1,27 @@
 /* jshint esversion: 6 */
-
 /** Login, socketId */
 const sockets = new Map();
 
 const socketsManager = () => {
+  const listSockets = () => {
+    return Array.from(sockets.keys()).join(", ");
+  };
+
   const register = data => {
     const result = sockets.set(data.socket, data.login = "");
     // TODO only when dev?
     if (result instanceof Map) {
       console.log("Socket registered.");
+      console.log(`Sockets: ${listSockets()}`);
     }
   };
+
 
   const unregister = data => {
     const result = sockets.delete(data.socket);
     if (result) {
       console.log("Socket unregistered.");
+      console.log(`Sockets: ${listSockets()}`);
     } else {
       console.log("Socket unregistered failure.");
     }
