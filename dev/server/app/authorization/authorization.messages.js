27cf6203b1921a28968a034c8aab20e8f36d15ba
Katarzyna Ziomek-Zdanowicz <katarzyna.ziomek.zdanowicz@gmail.com>
2018-04-24 13:41:23 +0200

    Strings into double quotes.

--- dev/server/app/authorization/authorization.messages.js
+++ dev/server/app/authorization/authorization.messages.js
@@ -1,8 +1,8 @@
 /* jshint esversion: 6 */
 module.exports = {
-  authorizeOption: data => `User ${data.login} being authorized via: ${data.option === "Number"? data.option + '(cookies)' : data.option}.`,
+  authorizeOption: data => `User ${data.login} being authorized via: ${data.option === "Number"? data.option + "(cookies)" : data.option}.`,
   authorizeSuccess: login => `User ${login} authorized.`,
   authorizePasswordFailure: login => `Incorrect ${login}'s password.'`,
   authorizeLoginFailure: login => `Login ${login} does not exist.'`,
-  authorizeDataFailure: data => `Something went wrong. The data is corrupted?`
+  authorizeDataFailure: data => "Something went wrong. The data is corrupted?"
 };
